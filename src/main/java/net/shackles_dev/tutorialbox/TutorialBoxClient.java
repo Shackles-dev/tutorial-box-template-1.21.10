cd9eb911d628e10363208f69262c650623739fe4
Shackles-dev <lucasclarke1001@gmail.com>
2025-12-03 00:16:52 +1000

    it didn't make an initial commit lol

---
+++ src/main/java/net/shackles_dev/tutorialbox/TutorialBoxClient.java
@@ -0,0 +1,11 @@
+package net.shackles_dev.tutorialbox;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialBoxClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
